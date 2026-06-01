58871b878a2205e53a9192b7a41383e82dbc1a26
Sai Tarrun Pitta <116748905+saitarrun@users.noreply.github.com>
2026-06-01 11:31:13 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,28 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # bruteforce 
+
+        # basecase if the len(s) != len(t) : False
+
+        # if len(s) != len(t) : return False
+
+        # # if the count of s == t: True 
+
+        # for char in set(s):
+        #     if s.count(char) != t.count(char):
+        #         return False
+        # return True
+
+        # counters
+
+        # return Counter(s) == Counter(t)
+
+        # sorted 
+
+        return sorted(s) == sorted(t)
+
+
+
+        
